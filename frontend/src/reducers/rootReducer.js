ffde85bcfd44759cadc956c1f1d91985805c9e3f
Nelli Leinonen <leinonen.nelli@gmail.com>
2020-02-29 18:36:49 +0200

    Rearrange the folder to push backend to Heroku

--- frontend/src/reducers/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import usernameReducer from './usernameReducer'
-import scoreReducer from './scoreReducer'
-import clickReducer from './clickReducer'
-
-export default combineReducers({ username: usernameReducer, score: scoreReducer, allClicks: clickReducer })
