5cdb969af8a8ba451eace021b08b15bab19991f1
Nelli Leinonen <leinonen.nelli@gmail.com>
2020-03-01 20:20:42 +0200

    Frontend build

---
+++ build/precache-manifest.52786f4e8a7bdc879b031a9cd88b0edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ca98106df140c5e4c518f02b42c017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a0d9ca0b2311222dacb",
+    "url": "/static/css/main.04dd16af.chunk.css"
+  },
+  {
+    "revision": "290932f0b756d42220a9",
+    "url": "/static/js/2.03e368c1.chunk.js"
+  },
+  {
+    "revision": "e1345d47b220a41451b8c23eb83bc290",
+    "url": "/static/js/2.03e368c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0d9ca0b2311222dacb",
+    "url": "/static/js/main.b678b82c.chunk.js"
+  },
+  {
+    "revision": "21836cbc3854bb657637",
+    "url": "/static/js/runtime-main.081e6bdd.js"
+  }
+]);
